c815be7c90205454eb4f33409ddab44350aca7d6
ClarenceMendoza <109626410+ClarenceMendoza@users.noreply.github.com>
2022-10-08 14:54:24 -0500

    Add files via upload
    10/082022
    Cambios Realizados al programa del automóvil
    Se modifico la lógica de programación POO
    Se segmentaron las clases 
    Se resolvió problema de ejecución
    
    Fix:3

---
+++ Problema2/src/problema/DistanciaRecorrida.java
@@ -0,0 +1,43 @@
+package problema;
+import java.util.Scanner;
+
+public class DistanciaRecorrida {
+
+		          public static void main( String[] args ) {
+			      double tiempo,  distancia = 0;
+			      double VI = 299792.458;
+			      double VF = 0.331;
+			   
+			      
+			      
+			     				  
+				  double  x1=0;
+				  Scanner v1 = new Scanner( System.in );
+				  System.out.println( "Introduce la distancia X1 : ");
+				  x1 = v1.nextInt();
+				  
+				  double x2 = 0;
+				  Scanner v2 = new Scanner( System.in );
+				  System.out.println( "Introduce la distancia X2 : ");
+				  tiempo = v2.nextInt();
+				  
+				  int y2 = 0;
+				  Scanner v3 = new Scanner( System.in );
+				  System.out.println( "Introduce la distancia Y1 : ");
+				  tiempo = v3.nextInt();
+				  
+				  int y1 = 0;
+				  Scanner v4 = new Scanner( System.in );
+				  System.out.println( "Introduce el tiempo : Y2 ");
+				  tiempo = v4.nextInt();
+				  
+				  distancia = Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y2 - y1, 2));
+			      System.out.printf( "La distancia recorrida es de: %.3f", distancia);
+		      
+	     	      
+	   
+	           
+	   }
+	      }   
+
+	      
---
+++ Problema2/src/problema/Problema.java
@@ -0,0 +1,26 @@
+package problema;
+import problema.Aceleracion;
+import problema.DistanciaRecorrida;
+import java.util.Scanner;
+
+
+
+class main {
+	
+	public static void main(String[] args) {
+	
+	
+		class Aceleracion {
+
+	}
+		class DistanciaRecorrida {
+	
+	
+	
+	}
+		
+	}
+	
+}
+
+
